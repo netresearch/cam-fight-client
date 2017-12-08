9bd9faf810133fe9f89d10fec9fca38ba7f5ca22
Andre Lademann <andre.lademann@netresearch.de>
2017-12-08 14:49:04 +0100

    Fixed waiting page redirects

--- src/components/ChallengeStatusChecker.vue
+++ src/components/ChallengeStatusChecker.vue
@@ -6,14 +6,8 @@
 
 export default {
   created() {
-    this.checkTeamCookie()
   },
   methods: {
-    checkTeamCookie() {
-      if (this.$cookies.isKey('team')) {
-        this.$router.push({ name: 'ChallengeList' })
-      }
-    }
   }
 }
 </script>
